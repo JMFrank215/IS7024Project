3113d186c68e14357973240cd23d5eb0c718b648
Sam <welshsr@mail.uc.edu>
2020-11-07 14:55:50 -0500

    Commit  - crime json data

--- IS7024Project/Pages/Index.cshtml.cs
+++ IS7024Project/Pages/Index.cshtml.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
+using QuickType;
 
 namespace IS7024Project.Pages
 {
@@ -19,6 +23,17 @@ namespace IS7024Project.Pages
 
         public void OnGet()
         {
+            using(var webClient = new WebClient())
+            {
+                string jsonString = webClient.DownloadString("https://raw.githubusercontent.com/JMFrank215/IS7024Project/master/PDI_Crime_Data.txt");
+                
+                
+                    var crime = Crime.FromJson(jsonString);
+                   
+                
+                ViewData["Crime"] = crime;
+            }
+           
 
         }
     }
