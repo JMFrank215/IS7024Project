52fff6a858d00de1a2326d22329b853d641b4c82
Sam <welshsr@mail.uc.edu>
2020-11-30 17:33:23 -0500

    Crime/Parks pages

---
+++ IS7024Project/Pages/CrimePage.cshtml.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
+using CrimeSpace;
+using ParkSpace;
+
+namespace IS7024Project.Pages
+{
+    public class CrimePageModel : PageModel
+    {
+        private readonly ILogger<CrimePageModel> _logger;
+
+        public CrimePageModel(ILogger<CrimePageModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            using (var webClient = new WebClient())
+            {
+                //Consuming Parks data
+                string crimesJSON = webClient.DownloadString("https://raw.githubusercontent.com/JMFrank215/IS7024Project/master/Parks_Data.txt");
+                var parks = Parks.FromJson(crimesJSON);
+                ViewData["Parks"] = parks;
+                ParkSpace.Parks[] Parksparks = ParkSpace.Parks.FromJson(crimesJSON);
+
+
+                //Consuming Crime data
+                string jsonString = webClient.DownloadString("https://raw.githubusercontent.com/JMFrank215/IS7024Project/master/PDI_Crime_Data.txt");
+
+
+                //Validation received data
+                string crimeschema = System.IO.File.ReadAllText("CrimeSchema.json");
+                JSchema cschema = JSchema.Parse(crimeschema);
+
+                JArray cjsonObject = JArray.Parse(jsonString);
+
+                if (cjsonObject.IsValid(cschema))
+                {
+                    var crimes = Crime.FromJson(jsonString);
+                    ViewData["Crime"] = crimes;
+
+                }
+
+            }
+
+
+        }
+    }
+}
