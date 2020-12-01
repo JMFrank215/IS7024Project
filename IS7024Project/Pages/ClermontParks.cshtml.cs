b17b5c2ada126cb4b3771ed7f6459d97876c5a37
Sam <welshsr@mail.uc.edu>
2020-12-01 00:20:06 -0500

    Added Crime and Park county-specific pages with dropdown menus for navigation

--- IS7024Project/Pages/ClermontParks.cshtml.cs
+++ IS7024Project/Pages/ClermontParks.cshtml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -13,11 +13,11 @@ using ParkSpace;
 
 namespace IS7024Project.Pages
 {
-    public class IndexModel : PageModel
+    public class ClermontParksModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        private readonly ILogger<ClermontParksModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public ClermontParksModel(ILogger<ClermontParksModel> logger)
         {
             _logger = logger;
         }
@@ -25,31 +25,12 @@ namespace IS7024Project.Pages
         public void OnGet()
         {
             using (var webClient = new WebClient())
-            { /*
-                //Consuming Weather data
-                String key = System.IO.File.ReadAllText("WeatherAPIKey.txt"); 
-                String weatherString = webClient.DownloadString("https://api.weatherbit.io/v2.0/current?&city=Cincinnati&country=USA&key=" + key);
-                WeatherSpace.Weather weatherWeather = WeatherSpace.Weather.FromJson(weatherString);
-
-                //precipitation
-                long precip = 0;
-                foreach(WeatherSpace.Datum weather in weatherWeather.Data)
-                {
-                    precip = weather.Precip;
-                }
-                if (precip < 1)
-                {
-                    ViewData["WeatherMessage"] = "Not looking like rain!";
-                } else
-                {
-                    ViewData["WeatherMessage"] = "Rain expected, be careful out there!";
-                } 
-
+            {
                 //Consuming Parks data
                 string crimesJSON = webClient.DownloadString("https://raw.githubusercontent.com/JMFrank215/IS7024Project/master/Parks_Data.txt");
                 var parks = Parks.FromJson(crimesJSON);
                 ViewData["Parks"] = parks;
-                ParkSpace.Parks[] Parksparks = ParkSpace.Parks.FromJson(crimesJSON); */
+                ParkSpace.Parks[] Parksparks = ParkSpace.Parks.FromJson(crimesJSON);
 
 
                 //Consuming Crime data
